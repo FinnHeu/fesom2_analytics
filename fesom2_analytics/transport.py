28a27b0787fc80069396a1abb2a09126b822be52
FinnHeu <finn.heukamp@t-online.de>
2021-04-14 11:37:59 +0200

    changed BSO coords

--- fesom2_analytics/transport.py
+++ fesom2_analytics/transport.py
@@ -63,7 +63,7 @@ def process_inputs(
 
             if section == "BSO":
                 section_start = (20, 79)
-                section_end = (20, 70.1)
+                section_end = (20, 69)
 
             elif section == "BSX":
                 section_start = (59.5, 80.0)

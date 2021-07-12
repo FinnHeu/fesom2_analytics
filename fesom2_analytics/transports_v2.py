ce96344cdc54379eeae41c88dc513aa34df27ebb
FinnHeu <Finn.Heukamp@t-online.de>
2021-07-12 13:03:08 +0200

    Added top level function

--- fesom2_analytics/transports_v2.py
+++ fesom2_analytics/transports_v2.py
@@ -611,4 +611,66 @@ def _ComputeTransports(ds, mesh, section, cell_intersections, section_waypoints,
     # SORTBY DISTANCE
     ds = ds.sortby('distances_to_start')
 
-    return ds
+    return
+
+
+def cross_section_transports(section, mesh_path, data_path, mesh_diag_path, years, use_great_circle=True, how='mean', add_extent=1, abg=[50, 15, -90]):
+    '''
+    cross_section_transports.py
+
+    Computes the horizontal transport across a vertical section from fesom2 velocity output on mesh elements, by computing the intersections
+    of the section with the elements.
+
+    Inputs
+    ------
+    section (list, str)
+        either a list of the form [lon_start, lon_end, lat_start, lat_end] or a string for a preset section: 'FRAMSTRAIT', 'BSO'
+    mesh_path (str)
+        directory where the mesh files are stored
+    data_path (str)
+        directory where the data is stored
+    mesh_diag_path (str: optional, default=None)
+        directory where the mesh_diag file is stored, if None it is assumed to be located in data_path
+    use_great_circle (bool)
+        compute the section waypoints along a great great circle (default=True)
+    how (str)
+        either 'mean' for time mean transport or 'ori' for original data (default='mean')
+    add_extent (int, float)
+        the additional extent of the cutoutbox [lon_start, lon_end, lat_start, lat_end],
+        choose as small as possible (small for high resolution meshes and large for low resolution meshes)
+        this will impove the speed of the function (default = 1°)
+    abg (list)
+        rotation of the velocity data (default=[50,15,-90])
+
+    Returns
+    -------
+    ds (xarray.Dataset)
+        dataset containing all output variables
+    section (dict)
+        dictionary containing all section information
+
+    '''
+
+    # Wrap the subfunctions up
+    mesh, mesh_diag, files, section = _ProcessInputs(
+        section, mesh_path, data_path, mesh_diag_path, years, how, use_great_circle)
+
+    section_waypoints, mesh, section, lon_rotation, rotation_flag_lon = _ComputeWaypoints(
+        section, mesh, use_great_circle)
+
+    elem_box_nods, elem_box_indices = _ReduceElementNumber(
+        section_waypoints, mesh, section, add_extent)
+
+    elem_box_nods, elem_box_indices, cell_intersections, intersection_coords = _LinePolygonIntersections(
+        mesh, section_waypoints, elem_box_nods, elem_box_indices)
+
+    distances_between, distances_to_start, layer_thickness, grid_cell_area = _CreateVerticalGrid(
+        intersection_coords, section, mesh)
+
+    ds = _CreateDataset(files, mesh, elem_box_indices, elem_box_nods,
+                        distances_between, distances_to_start, grid_cell_area, how, abg)
+
+    ds = _ComputeTransports(ds, mesh, section, cell_intersections,
+                            section_waypoints, use_great_circle, rotation_flag_lon, lon_rotation)
+
+    return ds, section
